de180ed6a38831693301c134aaaf45c10f69ca78
BuangSaDaplin <kitcalboneroflores@gmail.com>
2025-05-29 18:00:47 +0800

    Building basic widgets for the chat screen

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,102 +1,119 @@
-import 'dart:convert';
-import 'package:chat_app/models/image_model.dart';
-import 'package:chat_app/repo/image_repository.dart';
-import 'package:chat_app/services/auth_service.dart';
-
-import 'package:chat_app/widgets/chat_bubble.dart';
-import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
-import 'models/chat_message_entity.dart';
-import 'package:provider/provider.dart';
-
-class ChatPage extends StatefulWidget {
-  ChatPage({Key? key}) : super(key: key);
-
-  @override
-  State<ChatPage> createState() => _ChatPageState();
-}
-
-class _ChatPageState extends State<ChatPage> {
-  //initiate state of messages
-  List<ChatMessageEntity> _messages = [];
-
-  _loadInitialMessages() async {
-    rootBundle.loadString('assets/mock_messages.json').then((response) {
-      final List<dynamic> decodeList = jsonDecode(response) as List;
-
-      final List<ChatMessageEntity> _chatMessages = decodeList.map((listItem) {
-        return ChatMessageEntity.fromJson(listItem);
-      }).toList();
-
-      print(_chatMessages.length);
-
-      //final state of messages
-      setState(() {
-        _messages = _chatMessages;
-      });
-    }).then((_) {
-      print('Done!');
-    });
-
-    //print('Something');
-  }
-
-  onMessageSent(ChatMessageEntity entity) {
-    _messages.add(entity);
-    setState(() {});
-  }
-
-  //TODO: Move this to repository class
-
-  @override
-  void initState() {
-    _loadInitialMessages();
-
-    // TODO: implement initState
-    super.initState();
-  }
+class ChatPage extends StatelessWidget {
+  const ChatPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final username = context.watch<AuthService>().getUserName();
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: Text('Hi $username!'),
+        title: Text("Hi Leanne"),
         actions: [
           IconButton(
-              onPressed: () {
-                context.read<AuthService>().updateUserName("New Name!");
-              },
-              icon: Icon(Icons.change_circle)),
-          IconButton(
-              onPressed: () {
-                context.read<AuthService>().logoutUser();
-                Navigator.pushReplacementNamed(context, '/');
-                print('Icon press');
-              },
-              icon: Icon(Icons.logout))
+            onPressed: () {
+              print('Icon pressed');
+            },
+            icon: Icon(Icons.logout),
+          )
         ],
       ),
-      body: Column(
+      body: ListView(
         children: [
-          Expanded(
-              child: ListView.builder(
-                  itemCount: _messages.length,
-                  itemBuilder: (context, index) {
-                    return ChatBubble(
-                        alignment: _messages[index].author.userName ==
-                                context.read<AuthService>().getUserName()
-                            ? Alignment.centerRight
-                            : Alignment.centerLeft,
-                        entity: _messages[index]);
-                  })),
-          ChatInput(
-            onSubmit: onMessageSent,
+          Container(
+            margin: EdgeInsets.all(50),
+            padding: EdgeInsets.all(24),
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(12),
+                topRight: Radius.circular(12),
+                bottomLeft: Radius.circular(12),
+              ),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Hi, this is your message!',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+                Image.network(
+                  'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                  height: 200,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(50),
+            padding: EdgeInsets.all(24),
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(12),
+                topRight: Radius.circular(12),
+                bottomLeft: Radius.circular(12),
+              ),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Hi, this is your message!',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+                Image.network(
+                  'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                  height: 200,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(50),
+            padding: EdgeInsets.all(24),
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(12),
+                topRight: Radius.circular(12),
+                bottomLeft: Radius.circular(12),
+              ),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Hi, this is your message!',
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+                Image.network(
+                  'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                  height: 200,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 100,
+            decoration: BoxDecoration(
+              color: Colors.black,
+              borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+            ),
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.add, color: Colors.white),
+                ),
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.send, color: Colors.white),
+                ),
+              ],
+            ),
           ),
         ],
       ),
