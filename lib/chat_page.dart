731ca87a3c0f9dea40039ff658adc8915b7bd557
BuangSaDaplin <kitcalboneroflores@gmail.com>
2025-05-29 20:46:29 +0800

    Repository pattern

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:chat_app/models/image_model.dart';
+import 'package:chat_app/repo/image_repository.';
 import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
 import 'package:chat_app/widgets/chat_bubble.dart';
 import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
@@ -50,36 +50,20 @@ class _ChatPageState extends State<ChatPage> {
     setState(() {});
   }
 
-  Future<List<PixelfordImage>> _getNetworkImages() async {
-    var endpointUrl = Uri.parse('https://picsum.photos/v2/list');
+  //TODO: Move this to repository class
+  final ImageRepository _imageRepo = ImageRepository();
 
-    final response = await http.get(endpointUrl);
-
-    if (response.statusCode == 200) {
-      final List<dynamic> decodeList = jsonDecode(response.body) as List;
-
-      final List<PixelfordImage> _imageList = decodeList.map((listItem) {
-        return PixelfordImage.fromJson(listItem);
-      }).toList();
-      print(_imageList[0].urlFullSize);
-      return _imageList;
-    } else {
-      throw Exception('API not successful!');
-    }
-    }
 
 
 
   @override
   void initState() {
     _loadInitialMessages();
-    _getNetworkImages();
     // TODO: implement initState
     super.initState();
   }
   @override
   Widget build(BuildContext context) {
-    _getNetworkImages();
     final username = ModalRoute.of(context)!.settings.arguments as String;
     return Scaffold(
       appBar: AppBar(
@@ -101,7 +85,7 @@ class _ChatPageState extends State<ChatPage> {
       body: Column(
         children: [
           FutureBuilder<List<PixelfordImage>>(
-              future: _getNetworkImages(),
+              future: _imageRepo.getNetworkImages(),
               builder: (BuildContext context,
                   AsyncSnapshot<List<PixelfordImage>> snapshot) {
                 if (snapshot.hasData)
