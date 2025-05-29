08c88beed82796379f7bc00ee31917f33df9cefa
BuangSaDaplin <kitcalboneroflores@gmail.com>
2025-05-29 20:52:41 +0800

    Showing the selected image in ChatInput

--- lib/widgets/chat_input.dart
+++ lib/widgets/chat_input.dart
@@ -1,70 +1,100 @@
 import 'package:chat_app/models/chat_message_entity';
-import 'package:chat_app/widgets/picker_body';
+import 'package:chat_app/widgets/picker_body.dart';
 
 import "package:flutter/material.dart";
 
-class ChatInput extends StatelessWidget {
+class ChatInput extends StatefulWidget {
   final Function(ChatMessageEntity) onSubmit;
+
   ChatInput({Key? key, required this.onSubmit}) : super(key: key);
 
+  @override
+  State<ChatInput> createState() => _ChatInputState();
+}
+
+class _ChatInputState extends State<ChatInput> {
+  String _selectedImageUrl = '';
+
+  final chatMessageController = TextEditingController();
 
   void onSendButtonPressed(){
     print('ChatMessage: ${chatMessageController.text}');
-
     final newChatMessage = ChatMessageEntity(
         text: chatMessageController.text,
         id: "244",
         createdAt: DateTime.now().millisecondsSinceEpoch,
         author: Author(userName: 'poojab26'));
 
-    onSubmit(newChatMessage);
+    if (_selectedImageUrl.isNotEmpty) {
+      newChatMessage.imageUrl = _selectedImageUrl;
+    }
 
+    widget.onSubmit(newChatMessage);
+
+    chatMessageController.clear();
+    _selectedImageUrl = '';
+    setState(() {});
   }
 
+  void onImagePicked(String newImageUrl) {
+    setState(() {
+      _selectedImageUrl = newImageUrl;
+    });
+    Navigator.of(context).pop();
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100,
-      child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-          IconButton(
-            onPressed: () {
-              //TODO: Open a bottom sheet that shows a grid of images
-              showModalBottomSheet(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return NetworkImagePickerBody();
-                  });
-            },
-      icon: Icon(
-        Icons.add,
-        color: Colors.white,
-      ),
+        child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+        IconButton(
+        onPressed: () {
+      //TODO: Open a bottom sheet that shows a grid of images
+      showModalBottomSheet(
+          context: context,
+          builder: (BuildContext context) {
+            return NetworkImagePickerBody(
+              onImageSelected: onImagePicked,
+            );
+          });
+  },
+    icon: Icon(
+    Icons.add,
+    color: Colors.white,
+    ),
     ),
 
     Expanded(
-    child: TextField(
-    keyboardType: TextInputType.multiline,
-    maxLines: 5,
-    minLines: 1,
-    controller: chatMessageController,
-    textCapitalization: TextCapitalization.sentences,
-    style: TextStyle(color: Colors.white),
-    decoration: InputDecoration(
-    hintText: "Type your message",
-    hintStyle: TextStyle(color: Colors.blueGrey),
-    border: InputBorder.none),
-    )),
-    IconButton(
-      onPressed: onSendButtonPressed,
-      icon: Icon(
-          Icons.send, color: Colors.white
-      ),
-    ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              keyboardType: TextInputType.multiline,
+              maxLines: 5,
+              minLines: 1,
+              controller: chatMessageController,
+              textCapitalization: TextCapitalization.sentences,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                  hintText: "Type your message",
+                  hintStyle: TextStyle(color: Colors.blueGrey),
+                  border: InputBorder.none),
+            ),
+            if(_selectedImageUrl.isNotEmpty)
+              Image.network(_selectedImageUrl, height: 50,),
           ],
-      ),
+        )),
+          IconButton(
+            onPressed: onSendButtonPressed,
+            icon: Icon(
+                Icons.send, color: Colors.white
+            ),
+          ),
+        ],
+        ),
         decoration: BoxDecoration(
             color: Colors.black,
             borderRadius: BorderRadius.vertical(top: Radius.circular(20))
