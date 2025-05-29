d37108aece9322e8040d4c88d8732c2fd6d70fdc
BuangSaDaplin <kitcalboneroflores@gmail.com>
2025-05-29 18:39:22 +0800

    Writing your first stateful widget

--- lib/demo/counter_stateful_demo.dart
+++ lib/demo/counter_stateful_demo.dart
@@ -1,56 +1,40 @@
 import 'package:flutter/material.dart';
 
 class CounterStateful extends StatefulWidget {
-  //Widget variable should be final
-  final Color buttonColor;
-
-  CounterStateful({Key? key, required this.buttonColor}) : super(key: key);
+  const CounterStateful({super.key});
 
   @override
   State<CounterStateful> createState() => _CounterStatefulState();
 }
 
-class _CounterStatefulState extends State<CounterStateful> {
+class  _CounterStatefulState extends State<CounterStateful> {
+
   int counter = 0;
 
-  void increment() {
-    if (mounted) {
-      setState(() {
-        counter++;
-      });
-    }
+  void increment(){
+    setState(() {
+      counter++;
 
+    });
     print(counter);
   }
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-  }
 
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: widget.buttonColor,
-        child: Icon(Icons.add),
-        onPressed: () {
-          increment();
-        },
-      ),
-      body: Center(
-        child: Text(
-          '$counter',
-          style: TextStyle(fontSize: 30),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            increment();
+          },
         ),
-      ),
-    );
+        body: Center(
+          child: Text(
+            '$counter',
+            style: TextStyle(fontSize: 30),
+          ),
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
 import 'package:chat_app/chat_page.dart';
+import 'package:chat_app/counter_stateful_demo.dart';
 import 'package:chat_app/login_page.dart';
 import 'package:flutter/material.dart';
+
+
 
 void main() {
   runApp(ChatApp());
 }
 
-  class ChatApp extends StatelessWidget{
+
+class ChatApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-  // TODO: implement build
-  return MaterialApp(
-
-    title: "Chat App",
-    theme: ThemeData(primarySwatch: Colors.yellow),
-    home: const ChatPage(),
-  );
+    // TODO: implement build
+    return MaterialApp(
+        title: "Chat App",
+        theme: ThemeData(primarySwatch: Colors.yellow),
+      home: const CounterStateful(),
+    );
   }
-  }
+}
