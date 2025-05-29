375b432776cd3dee796349d6fc25c492d0f9476f
BuangSaDaplin <kitcalboneroflores@gmail.com>
2025-05-29 19:34:25 +0800

    Understanding the anatomy of a pubspec.yaml file

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
 import 'package:chat_app/chat_page.dart';
 import 'package:chat_app/login_page.dart';
-import 'package:chat_app/widgets/chat_input.dart';
 import 'package:flutter/material.dart';
-
-
+//import 'chat_page.dart';
+//import 'demo/counter_stateful_demo.dart';
+//import 'login_page.dart';
 
 void main() {
   runApp(ChatApp());
 }
-
 
 class ChatApp extends StatelessWidget {
   @override
@@ -16,11 +15,12 @@ class ChatApp extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
       title: "Chat App",
-      theme: ThemeData(primarySwatch: BrandColor.primaryColor,
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
           appBarTheme: AppBarTheme(
               backgroundColor: Colors.blue, foregroundColor: Colors.black)),
-      home:  LoginPage(),
-      routes: {'/chat' : (context) => ChatPage()},
+      home: LoginPage(),
+      routes: {'/chat': (context) => ChatPage()},
     );
   }
 }
